62aa823a8a69fa5a879de2e4c2c18d4d283977ee
Anurag145-prog <anuragvishwakarma802@gmail.com>
2025-11-20 21:32:51 +0530

    Add files via upload

---
+++ ass4.py
@@ -0,0 +1,9 @@
+a=float(input("enter measurement in inches:"))
+b=a/12
+c=a/36
+d=a*2.54
+e=a*0.0254
+print("inches into foot is:",b)
+print("inches into foot is:",c)
+print("inches into foot is:",d)
+print("inches into foot is:",e)  
